338f4b8fce4c76349ddcb88af4a8397ad9dd42b2
道化師 <wraith@trickpalace.net>
2017-07-28 22:30:33 +0900

    🛠 実装 ( WIP )

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,83 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+module WorkSpace
+{
+    export function getActiveDocument() : vscode.TextDocument | null
+    {
+        var activeTextEditor = vscode.window.activeTextEditor;
+        if (null !== activeTextEditor && undefined !== activeTextEditor)
+        {
+            var document = activeTextEditor.document;
+            if (null !== document && undefined !== document)
+            {
+                return document;
+            }
+        }
+        return null;
+    };
+}
+
+export module bomswitcher
+{
+    var indicator : vscode.StatusBarItem;
+
+    /*
+    function getConfiguration<type>(key?: string): type
+    {
+        var configuration = vscode.workspace.getConfiguration("bom-switcher");
+        return key ?
+            configuration[key] :
+            configuration;
+    }
+    */
+
+    export function registerCommand(context: vscode.ExtensionContext): void
+    {
+        indicator = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+        context.subscriptions.push(indicator);
+
+        context.subscriptions.push
+        (
+            vscode.commands.registerCommand
+            (
+                'bom-switcher.update', update
+            )
+        );
+        vscode.workspace.onDidChangeTextDocument
+        (
+            (_e_editor : vscode.TextDocumentChangeEvent) =>
+            {
+                update();
+            }
+        );
+        update();
+    }
+
+    export async function update() : Promise<void>
+    {
+        const document =  WorkSpace.getActiveDocument();
+        if (document)
+        {
+            const first = document.getText(new vscode.Range(new vscode.Position(0, 0),new vscode.Position(0, 1)));
+            indicator.text = "\u00A0" === first ? "💣": "🌀";
+        }
+        else
+        {
+            indicator.text = "🚧";
+            //indicator.color = color;
+        }
+        indicator.show();
+    }
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "bom-switcher" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    bomswitcher.registerCommand(context);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate()
+{
 }
